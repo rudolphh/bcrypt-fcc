455ee0813d2ae8fc08888486b503eaff11021ab1
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-05-12 00:04:37 -0700

    Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+  console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+      console.log(res); //true
+  });
+});
 
 
 //END_ASYNC
