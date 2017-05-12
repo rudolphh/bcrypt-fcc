d90b91fee74ad23ade1229eec910f15688a2698f
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-05-12 00:12:11 -0700

    Hash and Compare Passwords Synchronously

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+
 bcrypt.hash('passw0rd!', 13, (err, hash) => {
   console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
   bcrypt.compare('passw0rd!', hash, (err, res) => {
@@ -25,7 +26,11 @@ bcrypt.hash('passw0rd!', 13, (err, hash) => {
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
